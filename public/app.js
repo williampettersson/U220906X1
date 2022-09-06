445cff58f63815bf496bc20efc37e38791a6c0ab
williampettersson <william.c.pettersson@gmail.com>
2022-09-06 14:38:11 +0200

    Buttons added to calculator

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
 console.log('JS does work!');
 
 build();
+calculator();
+settings();
 
 function build() {
-
     const targetSettings = document.getElementById('app');
     const settings = document.createElement('div');
     settings.setAttribute('id', 'settings');
@@ -26,3 +27,60 @@ function build() {
     targetDivCalc.appendChild(divcalc);
 
 }
+
+function settings() {
+
+    const btnColor = document.getElementById('settings');
+    const color = document.createElement('button');
+    color.setAttribute('id', 'color');
+    color.innerText = 'Color';
+    btnColor.appendChild(color);
+
+
+}
+
+function calculator() {
+
+    const targetNumberInput1 = document.getElementById('calculator');
+    const numberInput1 = document.createElement('input');
+    numberInput1.setAttribute('id', 'number1');
+    numberInput1.setAttribute('name', 'number1');
+    targetNumberInput1.appendChild(numberInput1);
+
+    const btnAddition = document.getElementById('calculator');
+    const addition = document.createElement('button');
+    addition.setAttribute('id', 'addition');
+    addition.innerText = '+';
+    btnAddition.appendChild(addition);
+
+    const btnSubtraction = document.getElementById('calculator');
+    const subtraction = document.createElement('button');
+    subtraction.setAttribute('id', 'subtraction');
+    subtraction.innerText = '-';
+    btnSubtraction.appendChild(subtraction);
+
+    const btnMultiply = document.getElementById('calculator');
+    const multiply = document.createElement('button');
+    multiply.setAttribute('id', 'multiply');
+    multiply.innerText = '*';
+    btnMultiply.appendChild(multiply);
+
+    const btnDivision = document.getElementById('calculator');
+    const division = document.createElement('button');
+    division.setAttribute('id', 'division');
+    division.innerText = '*';
+    btnDivision.appendChild(division);
+
+    const targetNumberInput2 = document.getElementById('calculator');
+    const numberInput2 = document.createElement('input');
+    numberInput2.setAttribute('id', 'number2');
+    numberInput2.setAttribute('name', 'number2');
+    targetNumberInput2.appendChild(numberInput2); 
+
+    const btnClear = document.getElementById('calculator');
+    const clear = document.createElement('button');
+    clear.setAttribute('id', 'clear');
+    clear.innerText = 'Clear';
+    btnClear.appendChild(clear);
+}
+
